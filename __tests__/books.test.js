0b4dc544bcdc022959e0172df48748444e6f954c
Adam Robson <adamray312@gmail.com>
2022-11-10 15:23:09 -0800

    separation of concern

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('books routes', () => {
+describe('bowaoks routes', () => {
 
   beforeEach(() => {
     return setup(pool);
@@ -21,26 +21,6 @@ describe('books routes', () => {
     });
     expect(don).toHaveProperty('id', '1');
     expect(don).toHaveProperty('title', 'The Ingenious Gentleman Don Quixote of La Mancha');
-  });
-});
-
-describe('authors routes', () => {
-
-  beforeEach(() => {
-    return setup(pool);
-  });
-
-  test('return a list of authors', async () => {
-    const res = await request(app).get('/authors');
-    expect(res.body.length).toEqual(13);
-  });
-
-  test('return an author and detail', async () => {
-    const res = await request(app).get('/authors');
-    const writer = await res.body.find((author) => author.id === '3');
-    expect(writer).toHaveProperty('id', '3');
-    expect(writer).toHaveProperty('name', 'Ralph Ellison');
-    expect(writer).toMatchObject({ id: writer.id, name: writer.name });
   });
 
   afterAll(() => {
