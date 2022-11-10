f38ac1665d493b7c0fb0761cec209ffb5231507c
Adam Robson <adamray312@gmail.com>
2022-11-10 15:30:25 -0800

    add IF EXISTS to books and authors tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE books CASCADE;
-DROP TABLE authors CASCADE;
+DROP TABLE IF EXISTS books CASCADE;
+DROP TABLE IF EXISTS authors CASCADE;
 DROP TABLE IF EXISTS authors_books;
 
 CREATE TABLE books (
